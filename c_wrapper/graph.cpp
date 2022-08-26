d5546757de662a33fff4853f3a02f34b86a5702b
eeli1 <maierelias13@gmail.com>
2022-08-26 16:39:29 +0200

    compiling c

--- c_wrapper/graph.cpp
+++ c_wrapper/graph.cpp
@@ -1,26 +1,19 @@
-#include <ogdf/basic/graph_generators.h>
-#include <ogdf/planarity/SubgraphPlanarizer.h>
-#include <ogdf/planarity/PlanarSubgraphFast.h>
-#include <ogdf/planarity/VariableEmbeddingInserter.h>
-#include <ogdf/fileformats/GraphIO.h>
+#include "graph.h"
 
-using namespace ogdf;
-
-extern "C"
 void test()
 {
 	Graph G;
 	randomSimpleGraph(G, 100, 150);
 
-	// SubgraphPlanarizer SP;
-	// SP.setSubgraph(new PlanarSubgraphFast<int>);
-	// SP.setInserter(new VariableEmbeddingInserter);
-// 
-	// int crossNum;
-	// PlanRep PR(G);
-	// SP.call(PR, 0, crossNum);
-// 
-	// std::cout << crossNum << " crossings" << std::endl;
+	SubgraphPlanarizer SP;
+	SP.setSubgraph(new PlanarSubgraphFast<int>);
+	SP.setInserter(new VariableEmbeddingInserter);
+		
+	int crossNum;
+	PlanRep PR(G);
+	SP.call(PR, 0, crossNum);
+		
+	std::cout << crossNum << " crossings" << std::endl;
 	// GraphIO::write(PR, "output-plan.gml", GraphIO::writeGML);
 
 }
---
+++ c_wrapper/graph.h
@@ -0,0 +1,10 @@
+#include <ogdf/basic/graph_generators.h>
+#include <ogdf/planarity/SubgraphPlanarizer.h>
+#include <ogdf/planarity/PlanarSubgraphFast.h>
+#include <ogdf/planarity/VariableEmbeddingInserter.h>
+#include <ogdf/fileformats/GraphIO.h>
+
+using namespace ogdf;
+
+extern "C"
+void test();
